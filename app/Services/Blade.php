0f5dd35f41da6c3b95fba74aec0e54f4a9d234df
Fredy Martinez <fredy.martinez@gmail.com>
2023-08-28 17:09:32 -0500

    feat: se creo la clase Blade

---
+++ app/Services/Blade.php
@@ -0,0 +1,43 @@
+<?php
+namespace App\Services;
+
+use App\Services\Helpers;
+use eftec\bladeone\BladeOne;
+
+class Blade extends Helpers{
+
+    public function createView($template = "", $parametros = ""){
+
+        if($template == "") return "";
+
+        $blade = new BladeOne(URL_VIEWS, URL_CACHE,BladeOne::MODE_DEBUG);
+
+        $blade->pipeEnable=true;
+        $blade->setBaseUrl(URL_WEB);
+    
+        $userSession = auth();
+    
+        $dni = $userSession["dni"]??"";
+        $name = $userSession["name"]??"";
+        $email = $userSession["email"]??"";
+        $username = $userSession["username"]??"";
+        $role = $userSession["role"]??"";
+    
+        if($name != '') $blade->setAuth($name, $role);
+        
+        $menu = menu();
+        $currentPage = identifyCurrentPage($menu);
+    
+        
+        $camelcase = static function($arg=""){ 
+            return Helpers::camelcase($arg);
+        };
+    
+        $blade->directive('camelcase', $camelcase);
+        $blade->share("currentPage", $currentPage);
+    
+        return $blade->run($template, [...$parametros]);
+    }
+}
+
+?>
