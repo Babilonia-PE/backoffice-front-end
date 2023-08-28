5791de4e5fa9f1d9c495d5115c932f27ab401b13
Fredy Martinez <fredy.martinez@gmail.com>
2023-08-28 17:10:06 -0500

    feat: se añadio URL_WEB y TESTING como variables de entorno

--- config/app.php
+++ config/app.php
@@ -2,14 +2,19 @@
 session_start();
 
 define("URL_ROOT", $_SERVER["DOCUMENT_ROOT"] . "/");
+
+$dotenv = Dotenv\Dotenv::createImmutable(URL_ROOT);
+$dotenv->load(); 
+
 define("URL_APP", URL_ROOT . "app");
-define("URL_WEB", "https://backoffice.localhost/");
+define("URL_WEB", $_SERVER["URL_WEB"]??"http://localhost/backoffice/");
 define("URL_LOGS", URL_ROOT. "logs/");
 define("URL_PUBLIC", URL_ROOT . "public/");
 define("URL_ASSETS", URL_ROOT . "public/assets/");
 define("URL_API", URL_ROOT . "api/");
 define("URL_VIEWS", URL_ROOT. "views/");
 define("URL_CACHE", URL_ROOT. "cache/");
+define("TESTING", $_SERVER["TESTING"]??false);
 define("NOMBRE_APLICACION", "");
 define("AUTOR", "");
 
