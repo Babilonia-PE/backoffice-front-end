8e051a28df7d2992e96fd5817eeb6af528233a25
Fredy Martinez <fredy.martinez@gmail.com>
2023-08-28 17:10:46 -0500

    feat: se crearon las funciones de identifyCurrentPage, auth , menu

--- config/functions.php
+++ config/functions.php
@@ -1,8 +1,8 @@
 <?php
+use App\Services\Blade;
+use App\Services\Helpers;
 use eftec\bladeone\BladeOne;
-
-$dotenv = Dotenv\Dotenv::createImmutable(URL_ROOT);
-$dotenv->load();
+use App\Services\SesionService;
 
 if (!file_exists(URL_LOGS)) {
     mkdir(URL_LOGS);
@@ -30,15 +30,87 @@ function env($string = "")
 }
 
 function view($template = "", $parametros = []){
-    
-    if($template == "") return "";
-
-    $blade = new BladeOne(URL_VIEWS, URL_CACHE,BladeOne::MODE_DEBUG);
-    
-    return $blade->run($template, $parametros);   
+    $blade = new Blade();
+    return $blade->createView($template, $parametros);
 }
 
 function secure_asset($ruta){
     return URL_WEB . "public/". $ruta;
+}
+
+function identifyCurrentPage($menu){
+    
+    $identify = "home";
+    
+    $request_uri = $_SERVER["REQUEST_URI"]??"";
+    $request_uri = explode("/",$request_uri);
+    if($request_uri[0]=="") unset($request_uri[0]);
+    $request_uri = implode("/",$request_uri);
+    
+    foreach($menu as $item)
+    { 
+        $item_id = $item["id"]??0;
+        $item_alias = $item["url"]??"";
+
+        if($item_alias == $request_uri){
+            $identify = $item_id;
+        }
+    }
+    
+    return trim($identify);
+}
+
+function auth($param = ""){
+    
+    $userSession = SesionService::leer("correoUsuario") ?? [];
+
+    return $userSession[$param] ?? '';
+}
+
+function menu(){
+        
+    $menu = [];
+
+    $menu[0]["id"] = "alertas";
+    $menu[0]["url"] = "alertas";
+    $menu[0]["name"] = "Alertas";
+    $menu[0]["icon"] = "nav-icon fas fa-chart-pie";
+
+    $menu[1]["id"] = "avisos";
+    $menu[1]["url"] = "avisos";
+    $menu[1]["name"] = "Avisos";
+    $menu[1]["icon"] = "nav-icon fas fa-chart-pie";
+
+    $menu[2]["id"] = "clientes";
+    $menu[2]["url"] = "clientes";
+    $menu[2]["name"] = "Clientes";
+    $menu[2]["icon"] = "nav-icon fas fa-chart-pie";
+
+    $menu[3]["id"] = "leads";
+    $menu[3]["url"] = "leads";
+    $menu[3]["name"] = "Leads";
+    $menu[3]["icon"] = "nav-icon fas fa-chart-pie";
+
+    
+    $menu[4]["id"] = "views";
+    $menu[4]["url"] = "vistas";
+    $menu[4]["name"] = "Views";
+    $menu[4]["icon"] = "nav-icon fas fa-chart-pie";
+    
+    $menu[5]["id"] = "paquetes";
+    $menu[5]["url"] = "paquetes";
+    $menu[5]["name"] = "Paquetes";
+    $menu[5]["icon"] = "nav-icon fas fa-chart-pie";
+
+    $id = identifyCurrentPage($menu);
+
+    foreach($menu as $key=>$item)
+    {
+        $_id = $item["id"]??'';
+        
+        $menu[$key]["active"] = ($_id == $id) ? true : false;
+    }
+
+    return $menu;
 }
 ?>
