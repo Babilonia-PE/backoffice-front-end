569228c653c301458492a50d88c3587871bf97b0
Fredy Martinez <fredy.martinez@gmail.com>
2023-08-28 17:12:11 -0500

    feat: se integro base y cambio de nombre a title

--- views/includes/header.blade.php
+++ views/includes/header.blade.php
@@ -3,7 +3,8 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Dashboard</title>
+  @base
+  <title>BackOffice</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -33,9 +34,11 @@
 <div class="wrapper">
 
   <!-- Preloader -->
+  @if(TESTING)
   <div class="preloader flex-column justify-content-center align-items-center">
     <img class="animation__shake" src="{{ secure_asset("assets/img/AdminLTELogo.png") }}" alt="AdminLTELogo" height="60" width="60">
   </div>
+  @endif
 
   <!-- Navbar -->
   <nav class="main-header navbar navbar-expand navbar-white navbar-light">
@@ -45,10 +48,7 @@
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="index3.html" class="nav-link">Home</a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Contact</a>
+        <a href="/" class="nav-link">Inicio</a>
       </li>
     </ul>
 
