e437b518d3dc3db36ba2ec337517106abf0e27c2
Fredy <fmartinez.bpe@gmail.com>
2023-09-05 12:34:27 -0500

    fix: se cambio secure_assets por @asset

--- views/includes/footer.blade.php
+++ views/includes/footer.blade.php
@@ -16,17 +16,17 @@
 <!-- ./wrapper -->
 
 <!-- jQuery -->
-<script src="{{ secure_asset("plugins/jquery/jquery.min.js") }}"></script>
+<script src="@asset("plugins/jquery/jquery.min.js")"></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="{{ secure_asset("plugins/jquery-ui/jquery-ui.min.js") }}"></script>
+<script src="@asset("plugins/jquery-ui/jquery-ui.min.js")"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
   $.widget.bridge('uibutton', $.ui.button)
 </script>
 <!-- Bootstrap 4 -->
-<script src="{{ secure_asset("plugins/bootstrap/js/bootstrap.bundle.min.js") }}"></script>
+<script src="@asset("plugins/bootstrap/js/bootstrap.bundle.min.js")"></script>
 <!-- ChartJS -->
-<script src="{{ secure_asset("plugins/chart.js/Chart.min.js") }}"></script>
+<script src="@asset("plugins/chart.js/Chart.min.js")"></script>
 
 <!-- Sparkline -->
 <script>
@@ -34,31 +34,31 @@
 </script>
 @if($currentPage == "home")
 
-<script src="{{ secure_asset("plugins/sparklines/sparkline.js") }}"></script>
+<script src="@asset("plugins/sparklines/sparkline.js")"></script>
 
 <!-- JQVMap -->
-<script src="{{ secure_asset("plugins/jqvmap/jquery.vmap.min.js") }}"></script>
-<script src="{{ secure_asset("plugins/jqvmap/maps/jquery.vmap.usa.js") }}"></script>
+<script src="@asset("plugins/jqvmap/jquery.vmap.min.js")"></script>
+<script src="@asset("plugins/jqvmap/maps/jquery.vmap.usa.js")"></script>
 
 @endif
 <!-- jQuery Knob Chart -->
-<script src="{{ secure_asset("plugins/jquery-knob/jquery.knob.min.js") }}"></script>
+<script src="@asset("plugins/jquery-knob/jquery.knob.min.js")"></script>
 <!-- daterangepicker -->
-<script src="{{ secure_asset("plugins/moment/moment.min.js") }}"></script>
-<script src="{{ secure_asset("plugins/daterangepicker/daterangepicker.js") }}"></script>
+<script src="@asset("plugins/moment/moment.min.js")"></script>
+<script src="@asset("plugins/daterangepicker/daterangepicker.js")"></script>
 <!-- Tempusdominus Bootstrap 4 -->
-<script src="{{ secure_asset("plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js") }}"></script>
+<script src="@asset("plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js")"></script>
 <!-- Summernote -->
-<script src="{{ secure_asset("plugins/summernote/summernote-bs4.min.js") }}"></script>
+<script src="@asset("plugins/summernote/summernote-bs4.min.js")"></script>
 <!-- overlayScrollbars -->
-<script src="{{ secure_asset("plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js") }}"></script>
+<script src="@asset("plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js")"></script>
 <!-- AdminLTE App -->
-<script src="{{ secure_asset("assets/js/adminlte.js") }}"></script>
+<script src="@asset("js/adminlte.js")"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="{{ secure_asset("assets/js/demo.js") }}"></script>
+<script src="@asset("js/demo.js")"></script>
 @if($currentPage == "home")
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="{{ secure_asset("assets/js/pages/dashboard.js") }}"></script>
+<script src="@asset("js/pages/dashboard.js")"></script>
 @endif
 
 @yield("scripts")
--- views/login.blade.php
+++ views/login.blade.php
@@ -8,16 +8,18 @@
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{ secure_asset("plugins/fontawesome-free/css/all.min.css") }}">
+  <link rel="stylesheet" href="@asset("plugins/fontawesome-free/css/all.min.css")">
   <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="{{ secure_asset("plugins/icheck-bootstrap/icheck-bootstrap.min.css") }}">
+  <link rel="stylesheet" href="@asset("plugins/icheck-bootstrap/icheck-bootstrap.min.css")">
   <!-- Theme style -->
-  <link rel="stylesheet" href="{{ secure_asset("assets/css/adminlte.min.css") }}">
+  <link rel="stylesheet" href="@asset("css/adminlte.min.css")">
 </head>
-<body class="hold-transition login-page">
+<body class="hold-transition login-page bg-white">
 <div class="login-box">
   <div class="login-logo">
-    <a href="../../index2.html"><b>BACK</b>OFFICE</a>
+    <a href="/login">
+      <img src="@asset("img/logo.svg")" alt="">
+    </a>
   </div>
   @if(isset($message))
     <div class="alert alert-@if(isset($type)){{$type}}@else{{"warning"}}@endif " role="alert">
@@ -27,7 +29,7 @@
   <!-- /.login-logo -->
   <div class="card">
     <div class="card-body login-card-body">
-      <p class="login-box-msg">Sign in to start your session</p>
+      {{-- <p class="login-box-msg">Sign in to start your session</p> --}}
 
       <form action="/login" method="post">
         <div class="input-group mb-3">
@@ -47,16 +49,8 @@
           </div>
         </div>
         <div class="row">
-          <div class="col-8">
-            <div class="icheck-primary">
-              <input type="checkbox" id="remember">
-              <label for="remember">
-                Remember Me
-              </label>
-            </div>
-          </div>
           <!-- /.col -->
-          <div class="col-4">
+          <div class="col-12">
             <button type="submit" class="btn btn-primary btn-block">Sign In</button>
           </div>
           <!-- /.col -->
@@ -65,12 +59,6 @@
 
       <!-- /.social-auth-links -->
 
-      <p class="mb-1">
-        <a href="forgot-password.html">I forgot my password</a>
-      </p>
-      <p class="mb-0">
-        <a href="register.html" class="text-center">Register a new membership</a>
-      </p>
     </div>
     <!-- /.login-card-body -->
   </div>
@@ -78,11 +66,11 @@
 <!-- /.login-box -->
 
 <!-- jQuery -->
-<script src="{{ secure_asset("plugins/jquery/jquery.min.js") }}"></script>
+<script src="@asset("plugins/jquery/jquery.min.js")"></script>
 <!-- Bootstrap 4 -->
-<script src="{{ secure_asset("plugins/bootstrap/js/bootstrap.bundle.min.js") }}"></script>
+<script src="@asset("plugins/bootstrap/js/bootstrap.bundle.min.js")"></script>
 <!-- AdminLTE App -->
-<script src="{{ secure_asset("assets/js/adminlte.min.js") }}"></script>
+<script src="@asset("js/adminlte.min.js")"></script>
 <script>
   $("#hidenshowpassword").click(function(e){
     let input = document.getElementById("password");
