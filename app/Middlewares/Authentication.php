bbb4dd1ede3ee17dd9371138d7b51d11b7dcabf2
Fredy <fmartinez.bpe@gmail.com>
2023-09-12 21:14:01 -0500

    fix: se modifico middleware para consultar estado de login con 2fa

--- app/Middlewares/Authentication.php
+++ app/Middlewares/Authentication.php
@@ -2,20 +2,30 @@
 namespace App\Middlewares;
 
 use App\Services\SesionService;
+use App\Controllers\HelpersController;
 
 class Authentication{
 
-    public function auth(){
-        if (empty(SesionService::leer("correoUsuario"))) {
+    function __construct(){
+        $this->session_usuario = SesionService::leer("correoUsuario");
+        $this->approved = $this->session_usuario["approved"]??false;
+        $this->verifyAccountrFind = str_contains($_SERVER['REQUEST_URI'], "verify-account") ?? false;
+
+        HelpersController::tracer();
+    }
+    public function auth(){        
+        if ($this->verifyAccountrFind == false && ($this->approved==false || empty($this->session_usuario))) {
+            SesionService::destruir();
+            return redirect("login"); 
+        }else if($this->verifyAccountrFind == true && empty($this->session_usuario)){
             return redirect("login");
         }
     }
 
     public function noauth(){
-        if (!empty(SesionService::leer("correoUsuario"))) {
+        if (!empty($this->session_usuario) && $this->approved==true && $this->verifyAccountrFind==false) {
             return redirect();
         }
     }
-    
 }
 ?>
