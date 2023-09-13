9dc39e7de99d74f994f150e66ea3a8175e2b24b5
Fredy <fmartinez.bpe@gmail.com>
2023-09-13 12:07:42 -0500

    fix: se realizo el cambio de controllersHelpers por ServicesHelpers

--- app/Middlewares/Authentication.php
+++ app/Middlewares/Authentication.php
@@ -1,21 +1,22 @@
 <?php
 namespace App\Middlewares;
 
+use App\Services\Helpers;
 use App\Services\SesionService;
 use App\Controllers\AccountManager;
-use App\Controllers\HelpersController;
+
 
 class Authentication{
 
     function __construct(){
         $this->session_usuario = SesionService::leer("correoUsuario");
         $this->approved = $this->session_usuario["approved"]??false;
-        $this->verifyAccountrFind = HelpersController::getConstainsRequestURI("verify-account");
-        $this->updateAccountFind = HelpersController::getConstainsRequestURI("update-account-2fa");
+        $this->verifyAccountrFind = Helpers::getConstainsRequestURI("verify-account");
+        $this->updateAccountFind = Helpers::getConstainsRequestURI("update-account-2fa");
         $this->method = $_SERVER['REQUEST_METHOD'] ?? 'GET';
         $this->keyAuth2Store = AccountManager::verifySecondAuthSaved();
 
-        HelpersController::tracer();
+        Helpers::tracer();
 
     }
     public function auth(){        
