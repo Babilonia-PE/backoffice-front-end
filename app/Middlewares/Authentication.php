64f9c167a88d6c351084bab40b566d7dd06d0d6b
Fredy <fmartinez.bpe@gmail.com>
2023-09-13 11:49:38 -0500

    feat: se crearon los metodos para verificar las rutas privadas

--- app/Middlewares/Authentication.php
+++ app/Middlewares/Authentication.php
@@ -2,6 +2,7 @@
 namespace App\Middlewares;
 
 use App\Services\SesionService;
+use App\Controllers\AccountManager;
 use App\Controllers\HelpersController;
 
 class Authentication{
@@ -12,14 +13,25 @@ class Authentication{
         $this->verifyAccountrFind = HelpersController::getConstainsRequestURI("verify-account");
         $this->updateAccountFind = HelpersController::getConstainsRequestURI("update-account-2fa");
         $this->method = $_SERVER['REQUEST_METHOD'] ?? 'GET';
+        $this->keyAuth2Store = AccountManager::verifySecondAuthSaved();
+
         HelpersController::tracer();
+
     }
     public function auth(){        
-        
-        if($this->method=="GET" && $this->updateAccountFind==true && $this->approved==true && !empty($this->session_usuario) ){
+        /*SI LA VISTA ES REGISTRO DE 2FA SOLO SE MANTENDRA EN ESTA VISTA*/
+        /*
+        if(!$this->updateAccountFind && $_SERVER['REQUEST_URI'] != "/logout" && $this->keyAuth2Store == ""){
+            redirect("update-account-2fa");
+            return false;
+        }
+
+        if($this->method=="GET" && $this->updateAccountFind==true && $this->approved==true && !empty($this->session_usuario) && $this->keyAuth2Store!="" ){
             redirect();
             return false;
         }
+        
+        
 
         if (!$this->verifyAccountrFind && 
             !$this->updateAccountFind &&
@@ -29,11 +41,37 @@ class Authentication{
         }else if(!$this->verifyAccountrFind && empty($this->session_usuario)){
             return redirect("login");
         }
+        */
+        if(empty($this->session_usuario)){
+            return redirect("login");
+        }
     }
 
     public function noauth(){
         if (!empty($this->session_usuario) && $this->approved==true && $this->verifyAccountrFind==false) {
             return redirect();
+        }
+    }
+
+    public function verified(){
+        if(!$this->approved && !empty($this->session_usuario)){
+            return redirect("update-account-2fa");
+        }
+    }
+
+    public function VerifiedSaved(){
+        
+        if($this->keyAuth2Store!=""){
+            return redirect("verify-account");
+        }
+    }
+
+    public function VerifiedNoSaved(){
+        if(empty($this->session_usuario)){
+            return redirect("login");
+        }
+        else if($this->keyAuth2Store==""){
+            return redirect("update-account-2fa");
         }
     }
 }
