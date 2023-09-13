4acca3de742d9498aba8d73428dfdc556dab2228
Fredy <fmartinez.bpe@gmail.com>
2023-09-13 00:20:38 -0500

    fix: validacion update-account-2fa en rutas protegidas despues de login

--- app/Middlewares/Authentication.php
+++ app/Middlewares/Authentication.php
@@ -9,15 +9,24 @@ class Authentication{
     function __construct(){
         $this->session_usuario = SesionService::leer("correoUsuario");
         $this->approved = $this->session_usuario["approved"]??false;
-        $this->verifyAccountrFind = str_contains($_SERVER['REQUEST_URI'], "verify-account") ?? false;
-
+        $this->verifyAccountrFind = HelpersController::getConstainsRequestURI("verify-account");
+        $this->updateAccountFind = HelpersController::getConstainsRequestURI("update-account-2fa");
+        $this->method = $_SERVER['REQUEST_METHOD'] ?? 'GET';
         HelpersController::tracer();
     }
     public function auth(){        
-        if ($this->verifyAccountrFind == false && ($this->approved==false || empty($this->session_usuario))) {
+        
+        if($this->method=="GET" && $this->updateAccountFind==true && $this->approved==true && !empty($this->session_usuario) ){
+            redirect();
+            return false;
+        }
+
+        if (!$this->verifyAccountrFind && 
+            !$this->updateAccountFind &&
+            (!$this->approved || empty($this->session_usuario))) {
             SesionService::destruir();
             return redirect("login"); 
-        }else if($this->verifyAccountrFind == true && empty($this->session_usuario)){
+        }else if(!$this->verifyAccountrFind && empty($this->session_usuario)){
             return redirect("login");
         }
     }
