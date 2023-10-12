b360d3f0bc723a19d10bf6afcf26a0a9df19154b
Fredy <fmartinez.bpe@gmail.com>
2023-10-12 17:24:41 -0500

    feat: creacion de controlador Reportes

---
+++ app/Controllers/ReportesController.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Controllers;
+class ReportesController{
+
+    public function index(){
+        echo view("reportes");
+    }
+    public function post()
+    {
+
+    }
+}
+?>
