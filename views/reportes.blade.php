994cd54bf04d6f1837746c8262bc7b255437b30d
Fredy <fmartinez.bpe@gmail.com>
2023-10-12 18:17:24 -0500

    feat: obtener id de csv file

--- views/reportes.blade.php
+++ views/reportes.blade.php
@@ -11,92 +11,30 @@ Reportes
 @endsection
 
 @section('content')
+<div class="card card-secondary">
+    <div class="card-header">
+        <h3 class="card-title">Generar reporte de ID's</h3>
+    </div>
 
-<div class="row">
-    <div class="col-12">
-        <div class="card">
-            <div class="card-header">
-                
-                <div class="card-tools">
-                    <div class="input-group input-group-sm" style="width: 150px;">
-                        <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
-                        <div class="input-group-append">
-                            <button type="submit" class="btn btn-default">
-                                <i class="fas fa-search"></i>
-                            </button>
-                        </div>
-                    </div>
+    <div class="card-body">
+        <form>            
+            <div class="form-group">
+
+                <div class="custom-file">
+                    <input type="file" class="custom-file-input" id="inputCSV" accept=".csv" required>
+                    <label class="custom-file-label" for="inputCSV">Seleccione CSV</label>
                 </div>
-
             </div>
-
-            <div class="card-body table-responsive p-0">
-                <table class="table table-hover text-nowrap">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>User</th>
-                            <th>Date</th>
-                            <th>Status</th>
-                            <th>Reason</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @for($x=1;$x<6;$x++)
-                        <tr>
-                            <td>183</td>
-                            <td>John Doe</td>
-                            <td>11-7-2014</td>
-                            <td><span class="tag tag-success">Approved</span></td>
-                            <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                        </tr>
-                        <tr>
-                            <td>219</td>
-                            <td>Alexander Pierce</td>
-                            <td>11-7-2014</td>
-                            <td><span class="tag tag-warning">Pending</span></td>
-                            <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                        </tr>
-                        <tr>
-                            <td>657</td>
-                            <td>Bob Doe</td>
-                            <td>11-7-2014</td>
-                            <td><span class="tag tag-primary">Approved</span></td>
-                            <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                        </tr>
-                        <tr>
-                            <td>175</td>
-                            <td>Mike Doe</td>
-                            <td>11-7-2014</td>
-                            <td><span class="tag tag-danger">Denied</span></td>
-                            <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                        </tr>
-                        @endfor
-                    </tbody>
-                </table>
+            <div class="form-group">
+                    <button class="btn btn-primary">Generar</button>
             </div>
-
-        </div>
-
+        </form>
     </div>
-</div>
 
-<div class="row">
-    <div class="col-xs-12 col-sm-12 col-md-12">
-        <nav aria-label="Page navigation example">
-            <ul class="pagination">
-              <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-              <li class="page-item"><a class="page-link" href="#">1</a></li>
-              <li class="page-item"><a class="page-link" href="#">2</a></li>
-              <li class="page-item"><a class="page-link" href="#">3</a></li>
-              <li class="page-item"><a class="page-link" href="#">Next</a></li>
-            </ul>
-          </nav>
-    </div>
 </div>
 
 @endsection
 
 @section('scripts')
-
+<script src="@asset("js/reportes/generarExcel.js")"></script>
 @endsection
