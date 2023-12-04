7bfc05cfac0ebd0c6060ca2cab4da64284bdf27b
Fredy Martinez <fredy.martinez@gmail.com>
2023-12-04 17:30:13 -0500

    feat: implementacion de script de busqueda de clientes

--- views/clientes.blade.php
+++ views/clientes.blade.php
@@ -1,7 +1,78 @@
 @extends('Layout.master')
 
 @section('styles')
-
+<link rel="stylesheet" href="public/plugins/LibDataTables/DataTables-1.13.6/css/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" href="public/plugins/LibDataTables/Responsive-2.5.0/css/responsive.bootstrap4.min.css">
+<link rel="stylesheet" href="public/plugins/LibDataTables/Buttons-2.4.2/css/buttons.bootstrap4.min.css">
+<style>
+	
+    /*
+    @media(max-width: 575px) {
+		div.dtsp-panesContainer div.dtsp-searchPanes div.dtsp-searchPane {
+			max-width: 100%;
+		}
+    }
+	.dtsp-searchPane:first-child {
+		display: none;
+	}*/
+	div.dataTables_wrapper div.dataTables_processing {
+		position: fixed;
+		top: 30%!important;
+		left: 50%;
+		margin: 0;
+		z-index: 999;
+	}
+    .dataTables_wrapper .dt-buttons{
+        gap: .5rem;
+    }
+    .dataTables_wrapper .dataTables_length label{
+        margin: .5rem;
+    }
+    .table td, .table th {
+        padding: 0.3rem 0.5rem;
+    }
+	.dtr-bs-modal{
+		padding: 0!important;
+	}
+	span.dtr-data {
+		word-wrap: unset;
+		overflow-wrap: anywhere;
+		text-wrap: wrap;
+	}
+    @media(max-width: 575px) {
+        div.dataTables_wrapper div.dataTables_paginate ul.pagination {
+            justify-content: center;
+            flex-wrap: wrap;
+        }
+    }
+	.box-details{
+		display: flex;
+		flex-direction: row;
+		gap: 1rem;
+		align-items: center;
+    	padding: 0.3rem 0.5rem;
+	}
+	.box-details:not(:last-child) {
+		border-bottom: 1px solid #dee2e6;
+	}
+	.box-details > div{
+		overflow-wrap: anywhere;
+	}
+	.box-details > div:last-child {
+		flex: 1;
+	}
+	.box-details > div:first-child {
+		font-weight: 600;
+	}
+	
+    @media(max-width: 400px) {
+		.box-details{
+			flex-direction: column;
+			gap: 0rem;
+			align-items: start;
+        }
+    }
+</style>
 @endsection
 
 @section('page')
@@ -13,94 +84,439 @@ Clientes
 @section('content')
 
 <div class="row">
+	<div class="col-12">
+        <div id="filter_box" class="card collapsed-card">
+			<div class="card-header">
+                <h5 class="card-title">Filtros de búsqueda</h5>
+                <div class="card-tools">
+                  	<button type="button" class="btn btn-tool" data-card-widget="collapse">
+                    	<i id="icon_filter_box" class="fas fa-plus"></i>
+                  	</button>
+                </div>
+            </div>
+            <div class="card-body">
+				<div class="row align-items-end">
+              		<div class="col-md-4">
+                		<div class="form-group">
+                  			<label>RUC</label>
+                  			<input type="text" class="form-control" id="ruc" placeholder="RUC">
+                		</div>
+                	</div>
+              		<div class="col-md-4">
+                		<div class="form-group">
+                  			<label>Razón social</label>
+                  			<input type="text" class="form-control" id="razon_social" placeholder="Razón social">
+                		</div>
+                	</div>
+              		<div class="col-md-4">
+                		<div class="form-group">
+                  			<label>Nombre completo</label>
+                  			<input type="text" class="form-control" id="name" placeholder="Nombre completo">
+                		</div>
+                	</div>
+              		<div class="col-md-4">
+                		<div class="form-group">
+                  			<label>Correo</label>
+                  			<input type="email" class="form-control" id="email" placeholder="Correo">
+                		</div>
+                	</div>
+					<div class="col-md-4">
+						<div class="form-group">
+							<label for="exampleInputEmail1">Fecha de creación</label>
+							<div class="form-row">
+								<div class="col-6">
+									<input type="date" class="form-control" id="date_from" placeholder="Desde">
+								</div>
+								<div class="col-6">
+									<input type="date" class="form-control" id="date_to" placeholder="Hasta">
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="form-group">
+							<div class="form-row">
+								<div class="col">
+									<button id="applyfiltters" type="button" class="btn btn-primary btn-block"><i class="fas fa-filter"></i> Aplicar filtros</button>
+								</div>
+								<div class="col-auto">
+									<button id="removefiltters" type="button" class="btn btn-secondary"><i class="fas fa-trash"></i></button>
+								</div>
+							</div>
+						</div>
+					</div>
+                </div>
+			</div>
+        </div>
+	</div>
     <div class="col-12">
         <div class="card">
-            <div class="card-header">
-                
-                <div class="card-tools">
-                    <div class="input-group input-group-sm" style="width: 150px;">
-                        <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
-                        <div class="input-group-append">
-                            <button type="submit" class="btn btn-default">
-                                <i class="fas fa-search"></i>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-
-            <div class="card-body table-responsive p-0">
-                <table class="table table-hover text-nowrap">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>User</th>
-                            <th>Date</th>
-                            <th>Status</th>
-                            <th>Reason</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @for($x=1;$x<6;$x++)
-                        <tr>
-                            <td>183</td>
-                            <td>John Doe</td>
-                            <td>11-7-2014</td>
-                            <td><span class="tag tag-success">Approved</span></td>
-                            <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                        </tr>
-                        <tr>
-                            <td>219</td>
-                            <td>Alexander Pierce</td>
-                            <td>11-7-2014</td>
-                            <td><span class="tag tag-warning">Pending</span></td>
-                            <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                        </tr>
-                        <tr>
-                            <td>657</td>
-                            <td>Bob Doe</td>
-                            <td>11-7-2014</td>
-                            <td><span class="tag tag-primary">Approved</span></td>
-                            <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                        </tr>
-                        <tr>
-                            <td>175</td>
-                            <td>Mike Doe</td>
-                            <td>11-7-2014</td>
-                            <td><span class="tag tag-danger">Denied</span></td>
-                            <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                        </tr>
-                        @endfor
-                    </tbody>
+            <div class="card-body table-responsive">
+				<p class="text-center" name="loading"><img src="public/assets/img/loading.gif" width="50" /></p>
+                <table class="display table table-striped nowrap compact responsive d-none" cellspacing="0" width="100%">
+                    
                 </table>
             </div>
-
         </div>
-
     </div>
 </div>
 
-<div class="row">
-    <div class="col-xs-12 col-sm-12 col-md-12">
-        <nav aria-label="Page navigation example">
-            <ul class="pagination">
-              <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-              <li class="page-item"><a class="page-link" href="#">1</a></li>
-              <li class="page-item"><a class="page-link" href="#">2</a></li>
-              <li class="page-item"><a class="page-link" href="#">3</a></li>
-              <li class="page-item"><a class="page-link" href="#">Next</a></li>
-            </ul>
-          </nav>
-    </div>
+
+<!-- Modal -->
+<div class="modal fade p-0" id="rowDetails" tabindex="-1" role="dialog" aria-labelledby="rowDetailsLabel" aria-hidden="true">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+        		<h5 class="modal-title">Detalles para</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+				<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body">
+				<div class="box-details"></div>
+			</div>
+		</div>
+	</div>
 </div>
 
 @endsection
 
 @section('scripts')
+<script src="public/plugins/LibDataTables/datatables.min.js"></script>
+<script src="public/plugins/LibDataTables/DataTables-1.13.6/js/dataTables.bootstrap4.min.js"></script>
+<script src="public/plugins/LibDataTables/Responsive-2.5.0/js/responsive.bootstrap4.min.js"></script>
+<script src="public/plugins/LibDataTables/Buttons-2.4.2/js/buttons.bootstrap4.min.js"></script>
+<script src="public/plugins/LibDataTables/JSZip-3.10.1/jszip.min.js"></script>
+<script src="public/plugins/LibDataTables/Buttons-2.4.2/js/buttons.html5.min.js"></script>
+<script src="public/plugins/LibDataTables/Buttons-2.4.2/js/buttons.colVis.min.js"></script>
+<script>
+    let tableSaved = null;
+	let dtDraw = 1;
+	let filters = [];
+	
+	const headers = [
+			{ "title" : "ID del usuario" },
+			{ "title": "Nombre Completo" },
+			{ "title": "Correo electronico" },
+			{ "title": "Teléfono" },
+			{ "title": "Razon social" },
+			{ "title": "Nombre comercial" },
+			{ "title": "RUC" },
+			{ "title": "Dirección" },
+			{ "title": "Descripción" },
+			{ "title": "Colecciones" },
+			{ "title": "Interesados" },
+			{ "title": "Avisos" },
+			{ "title": "Proyectos" },
+			{ "title": "Estadísticas" },
+			{ "title": "resumen" },
+			{ "title": "Fecha y hora de creación del perfil" },
+			{ "title": "Acciones" }
+		]
+    const lenguaje = {
+		"processing": "Procesando...",
+		"lengthMenu": "Mostrar _MENU_ registros",
+		"zeroRecords": "No se encontraron resultados",
+		"emptyTable": "Ningún dato disponible en esta tabla",
+		"infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+		"infoFiltered": "(filtrado de un total de _MAX_ registros)",
+		"search": "Buscar:",
+		"infoThousands": ",",
+		"loadingRecords": "Cargando...",
+		"paginate": {
+			"first": "Primero",
+			"last": "Último",
+			"next": "Siguiente",
+			"previous": "Anterior"
+		},
+		"aria": {
+			"sortAscending": ": Activar para ordenar la columna de manera ascendente",
+			"sortDescending": ": Activar para ordenar la columna de manera descendente"
+		},
+		"buttons": {
+			"copy": "Copiar",
+			"colvis": "Visibilidad",
+			"collection": "Colección",
+			"colvisRestore": "Restaurar visibilidad",
+			"copyKeys": "Presione ctrl o u2318 + C para copiar los datos de la tabla al portapapeles del sistema. <br \/> <br \/> Para cancelar, haga clic en este mensaje o presione escape.",
+			"copySuccess": {
+				"1": "Copiada 1 fila al portapapeles",
+				"_": "Copiadas %ds fila al portapapeles"
+			},
+			"copyTitle": "Copiar al portapapeles",
+			"csv": "CSV",
+			//"excel": "Excel",
+			"excel": "Exportar página actual a Excel",
+			"pageLength": {
+				"-1": "Mostrar todas las filas",
+				"_": "Mostrar %d filas"
+			},
+			"pdf": "PDF",
+			"print": "Imprimir",
+			"renameState": "Cambiar nombre",
+			"updateState": "Actualizar",
+			"createState": "Crear Estado",
+			"removeAllStates": "Remover Estados",
+			"removeState": "Remover",
+			"savedStates": "Estados Guardados",
+			"stateRestore": "Estado %d"
+		},
+		"autoFill": {
+			"cancel": "Cancelar",
+			"fill": "Rellene todas las celdas con <i>%d<\/i>",
+			"fillHorizontal": "Rellenar celdas horizontalmente",
+			"fillVertical": "Rellenar celdas verticalmentemente"
+		},
+		"decimal": ",",
+		"searchBuilder": {
+			"add": "Añadir filtro",
+			"button": {
+				"0": "Constructor de búsqueda",
+				"_": "Constructor de búsqueda (%d)"
+			},
+			"clearAll": "Borrar todo",
+			"condition": "Condición",
+			"conditions": {
+				"date": {
+					"after": "Despues",
+					"before": "Antes",
+					"between": "Entre",
+					"empty": "Vacío",
+					"equals": "Igual a",
+					"notBetween": "No entre",
+					"notEmpty": "No Vacio",
+					"not": "Diferente de"
+				},
+				"number": {
+					"between": "Entre",
+					"empty": "Vacio",
+					"equals": "Igual a",
+					"gt": "Mayor a",
+					"gte": "Mayor o igual a",
+					"lt": "Menor que",
+					"lte": "Menor o igual que",
+					"notBetween": "No entre",
+					"notEmpty": "No vacío",
+					"not": "Diferente de"
+				},
+				"string": {
+					"contains": "Contiene",
+					"empty": "Vacío",
+					"endsWith": "Termina en",
+					"equals": "Igual a",
+					"notEmpty": "No Vacio",
+					"startsWith": "Empieza con",
+					"not": "Diferente de",
+					"notContains": "No Contiene",
+					"notStarts": "No empieza con",
+					"notEnds": "No termina con"
+				},
+				"array": {
+					"not": "Diferente de",
+					"equals": "Igual",
+					"empty": "Vacío",
+					"contains": "Contiene",
+					"notEmpty": "No Vacío",
+					"without": "Sin"
+				}
+			},
+			"data": "Data",
+			"deleteTitle": "Eliminar regla de filtrado",
+			"leftTitle": "Criterios anulados",
+			"logicAnd": "Y",
+			"logicOr": "O",
+			"rightTitle": "Criterios de sangría",
+			"title": {
+				"0": "Constructor de búsqueda",
+				"_": "Constructor de búsqueda (%d)"
+			},
+			"value": "Valor"
+		},
+		"searchPanes": {
+			"clearMessage": "Borrar todo",
+			"collapse": {
+				"0": "Paneles de búsqueda",
+				"_": "Paneles de búsqueda (%d)"
+			},
+			"count": "{total}",
+			"countFiltered": "{shown} ({total})",
+			"emptyPanes": "Sin paneles de búsqueda",
+			"loadMessage": "Cargando paneles de búsqueda",
+			"title": "Filtros Activos - %d",
+			"showMessage": "Mostrar Todo",
+			"collapseMessage": "Colapsar Todo"
+		},
+		"select": {
+			"cells": {
+				"1": "1 celda seleccionada",
+				"_": "%d celdas seleccionadas"
+			},
+			"columns": {
+				"1": "1 columna seleccionada",
+				"_": "%d columnas seleccionadas"
+			},
+			"rows": {
+				"1": "1 fila seleccionada",
+				"_": "%d filas seleccionadas"
+			}
+		},
+		"thousands": ".",
+		"datetime": {
+			"previous": "Anterior",
+			"next": "Proximo",
+			"hours": "Horas",
+			"minutes": "Minutos",
+			"seconds": "Segundos",
+			"unknown": "-",
+			"amPm": [
+				"AM",
+				"PM"
+			],
+			"months": {
+				"0": "Enero",
+				"1": "Febrero",
+				"10": "Noviembre",
+				"11": "Diciembre",
+				"2": "Marzo",
+				"3": "Abril",
+				"4": "Mayo",
+				"5": "Junio",
+				"6": "Julio",
+				"7": "Agosto",
+				"8": "Septiembre",
+				"9": "Octubre"
+			},
+			"weekdays": [
+				"Dom",
+				"Lun",
+				"Mar",
+				"Mie",
+				"Jue",
+				"Vie",
+				"Sab"
+			]
+		},
+		"editor": {
+			"close": "Cerrar",
+			"create": {
+				"button": "Nuevo",
+				"title": "Crear Nuevo Registro",
+				"submit": "Crear"
+			},
+			"edit": {
+				"button": "Editar",
+				"title": "Editar Registro",
+				"submit": "Actualizar"
+			},
+			"remove": {
+				"button": "Eliminar",
+				"title": "Eliminar Registro",
+				"submit": "Eliminar",
+				"confirm": {
+					"_": "¿Está seguro que desea eliminar %d filas?",
+					"1": "¿Está seguro que desea eliminar 1 fila?"
+				}
+			},
+			"error": {
+				"system": "Ha ocurrido un error en el sistema (<a target=\"\\\" rel=\"\\ nofollow\" href=\"\\\">Más información&lt;\\\/a&gt;).<\/a>"
+			},
+			"multi": {
+				"title": "Múltiples Valores",
+				"info": "Los elementos seleccionados contienen diferentes valores para este registro. Para editar y establecer todos los elementos de este registro con el mismo valor, hacer click o tap aquí, de lo contrario conservarán sus valores individuales.",
+				"restore": "Deshacer Cambios",
+				"noMulti": "Este registro puede ser editado individualmente, pero no como parte de un grupo."
+			}
+		},
+		"info": "Mostrando _START_ a _END_ de _TOTAL_ registros",
+		"stateRestore": {
+			"creationModal": {
+				"button": "Crear",
+				"name": "Nombre:",
+				"order": "Clasificación",
+				"paging": "Paginación",
+				"search": "Busqueda",
+				"select": "Seleccionar",
+				"columns": {
+					"search": "Búsqueda de Columna",
+					"visible": "Visibilidad de Columna"
+				},
+				"title": "Crear Nuevo Estado",
+				"toggleLabel": "Incluir:"
+			},
+			"emptyError": "El nombre no puede estar vacio",
+			"removeConfirm": "¿Seguro que quiere eliminar este %s?",
+			"removeError": "Error al eliminar el registro",
+			"removeJoiner": "y",
+			"removeSubmit": "Eliminar",
+			"renameButton": "Cambiar Nombre",
+			"renameLabel": "Nuevo nombre para %s",
+			"duplicateError": "Ya existe un Estado con este nombre.",
+			"emptyStates": "No hay Estados guardados",
+			"removeTitle": "Remover Estado",
+			"renameTitle": "Cambiar Nombre Estado"
+		}
+	};
+	const Buttons = [
+		{
+			extend: "excelHtml5",
+			exportOptions: {
+				//columns: ":not(:last-child)",
+				columns: function(idx, data, node) {
+					return $('table').DataTable().column(idx).visible();
+				}
+			}
+		},
+		{
+			extend: 'colvis',
+            columns: ':not(.noVis)',
+			text: "Ocultar columnas"
+		}
+	];
+	//POBLAR FILTROS
+	jQuery.fn.populatefilters=function(){
+		if (localStorage.getItem('filter_clientes') !== null) {
+			const filter_clientes = JSON.parse(localStorage.getItem('filter_clientes'));
+			$("#ruc").val(filter_clientes.ruc??'');
+			$("#razon_social").val(filter_clientes.razon_social??'');
+			$("#name").val(filter_clientes.name??'');
+			$("#email").val(filter_clientes.email??'');
+			$("#date_from").val(filter_clientes.date?.from??'');
+			$("#date_to").val(filter_clientes.date?.to??'');
+			$("#filter_box").removeClass("collapsed-card");
+			$("#icon_filter_box").addClass("fa-minus").removeClass("fa-plus");
+		}
+    };
+    //REDIMENSIONAR DATATABLE
+	jQuery.fn.redimensionarTable=function(){
+		if(tableSaved != null){
+			tableSaved.responsive.recalc();
+		}
+    };
+    jQuery.fn.createDataTable=function(searchBuilder = null, columnDefs = null, returnTable = {}, lengthMenu = 25, dom = true, columns = null, data = null){
+		var attr = ( !jQuery.isEmptyObject(returnTable) && returnTable.hasOwnProperty('attr') ) ? returnTable.attr:'class';
+		var value = ( !jQuery.isEmptyObject(returnTable) && returnTable.hasOwnProperty('value') ) ? returnTable.value:'display';
+		var dom = ( dom ) ? 'Blrtip':'lfrtip'; //f: buscador
+		var element =  ( attr == 'class' ) ? $('table.'+ value):$('table['+ attr +'="'+ value +'"]');
+		var table = element
+		//.on( 'search.dt', function () { $(this).redimensionarTable(); } )
+		//.on( 'page.dt', function () { $(this).redimensionarTable(); } )
+		
+		.on('preXhr.dt', function ( e, settings, data ) {
+			
+			//console.log(data);
+			//console.log(data['searchBuilder']);
+			//delete data.searchBuilder;
+		} )
+		.DataTable({
+			dom: dom,
+			buttons: Buttons,
+			"lengthMenu": [ ( (lengthMenu == 25) ? 25:lengthMenu ), 50, 75, 100, 200, 500 ],
+			"pageLength": lengthMenu,
+			"order": [],
+			//"data": data,
 
 			ajax: {
-				"url": 'https://services-testing.babilonia.io/app/user/users',
+				"url": 'https://services-testing.babilonia.io/app/users/users',
 				"type": 'GET',
 				"data": function ( data ) {
 					filters = [];
