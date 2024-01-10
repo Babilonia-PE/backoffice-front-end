7791d9985aa109eabb98baa03e855299c82a8a7e
codigoinerte <fmartinez.bpe@gmail.com>
2024-01-10 17:52:59 -0500

    feat: se obtuvo la lista plana de la configuración de permisos del menu

--- app/Controllers/ConfigurationPermissionsController.php
+++ app/Controllers/ConfigurationPermissionsController.php
@@ -7,8 +7,16 @@ use App\Services\SesionService;
 
 class ConfigurationPermissionsController{
 
+    public $actions = [];
+
     public function __construct(){
-        $this->currentPage = "configuration-permisos";                    
+        $this->currentPage = "configuration-permisos";
+        $this->actions = [
+            "view" => "Ver",
+            "create" => "Crear",
+            "update" => "Editar",
+            "delete" => "Eliminar"
+        ];
     }
 
     public function index(){
@@ -25,13 +33,14 @@ class ConfigurationPermissionsController{
 
     public function permissionDetail($id = null){
 
-        $menuFlatten = $this->getPermissions();
+        $Permissions = $this->getPermissions();
         
-        dd($menuFlatten);
-
         echo view("configuracion-permissions-detalle", [
             "currentPage" => $this->currentPage,
-            "data" => []
+            "data" => [
+                "permissions" => $Permissions,
+                "actions" => $this->actions
+            ]
         ]);
     }
 
@@ -64,14 +73,63 @@ class ConfigurationPermissionsController{
         }else{
             $menudb = "[]";
         }
-        
+
         $menuStore = json_decode($menudb, true)??[];
         
         foreach($menuStore as $item){
            menu_item($item);            
         }
+
+        $menu = $GLOBALS["menu"];
+
+        # set nuevo menu a array de permisos
+
+        if(file_exists(URL_ROOT."db/permissionsstore.json")){
+            $permissionsdb = file_get_contents(URL_ROOT."db/permissionsstore.json");
+        }else{
+            $permissionsdb = "[]";
+        }
+
+        $permissionsdb = json_decode($permissionsdb, true)??[];
+
+        if(count($menu) > 0){
+            foreach($menu as $k2 => $y){
+                $m_id = $y["id"] ?? '';
+                $stop = false;
+
+                if(count($permissionsdb) > 0){
+                    foreach($permissionsdb as $k1 => $x){
+                        $p_id = $x["id"] ?? '';
         
-        return $GLOBALS["menu"];
+                        foreach($this->actions as $ak => $action){
+                            if(!isset($permissionsdb[$k1][$ak])) $permissionsdb[$k1][$ak] = null;
+                        }
+
+                        if($p_id == $m_id){
+                            $stop = true;
+                            break;
+                        }                            
+                    }
+                }
+
+                if($stop){
+                    unset($menu[$k2]);
+                    continue;
+                }
+
+                unset($menu[$k2]["icon"]);
+                unset($menu[$k2]["state"]);
+
+                foreach($this->actions as $ak => $action){
+                    $menu[$k2][$ak] = null;
+                }
+                
+            }
+        }
+
+        $menu = array_merge($permissionsdb, $menu);
+        
+        return $menu;
     }
 
 
