caa4d74c6268b2adc7458ec060f48c89644b478b
codigoinerte <fmartinez.bpe@gmail.com>
2024-01-10 17:29:58 -0500

    feat: flatten menu

--- app/Controllers/ConfigurationPermissionsController.php
+++ app/Controllers/ConfigurationPermissionsController.php
@@ -1,13 +1,14 @@
 <?php
 namespace App\Controllers;
 
+use RecursiveArrayIterator;
+use RecursiveIteratorIterator;
 use App\Services\SesionService;
 
 class ConfigurationPermissionsController{
 
     public function __construct(){
-        $this->currentPage = "configuration-permisos";                
-                
+        $this->currentPage = "configuration-permisos";                    
     }
 
     public function index(){
@@ -22,12 +23,57 @@ class ConfigurationPermissionsController{
         
     }
 
-    public function permissionDetail(){
+    public function permissionDetail($id = null){
+
+        $menuFlatten = $this->getPermissions();
+        
+        dd($menuFlatten);
 
         echo view("configuracion-permissions-detalle", [
             "currentPage" => $this->currentPage,
             "data" => []
         ]);
     }
+
+    public function getPermissions(){
+        
+        $GLOBALS["menu"] = [];
+
+        function menu_item($array = []){
+
+            global $menu;
+
+            $children = $array["children"] ?? [];
+            
+            if(isset($array["children"])) unset($array["children"]);
+
+            array_push($menu, $array);
+    
+            if(count($children)>0)
+            {            
+                foreach($children as $item)
+                {
+                    menu_item($item);                                        
+                }
+                
+            }            
+        }
+        
+        if(file_exists(URL_ROOT."db/menustore.json")){
+            $menudb = file_get_contents(URL_ROOT."db/menustore.json");
+        }else{
+            $menudb = "[]";
+        }
+        
+        $menuStore = json_decode($menudb, true)??[];
+        
+        foreach($menuStore as $item){
+           menu_item($item);            
+        }
+        
+        return $GLOBALS["menu"];
+    }
+
+
 }
 ?>
