9d289008b475781196fb6c4a81ef0ce141f1853e
codigoinerte <fmartinez.bpe@gmail.com>
2024-01-12 13:25:22 -0500

    fix: creación de componente select

---
+++ views/components/select.blade.php
@@ -0,0 +1,19 @@
+@php
+    $data = isset($data) && !empty($data)? $data: [];
+    $class = isset($class) && !empty($class)? $class: "";
+    $id = isset($id) && !empty($id)? $id: "";
+    $first = isset($first) && !empty($first)? $first: false;
+    $n = 0;
+
+    $data = unsetArray($data, 0, !$first);
+    
+@endphp
+<select name="{{ $id }}" id="{{ $id }}" class="form-control select2" title="{{ $placeholder }}" placeholder="{{ $placeholder }}" style="width: 100%;">
+    <option selected disabled value="">Elige una opción</option>
+    @foreach ($data as $k => $item)
+        <option value="{{ $k }}">{{ $item }}</option>
+    @endforeach
+</select>
+@php
+ $n++;
+@endphp
