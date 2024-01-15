dfbfe744a696d9991bc87091f7649b27ae528e39
Fredy Martinez <fredy.martinez@gmail.com>
2024-01-15 09:48:27 -0500

    fix: importación de store

--- app/Controllers/ConfigurationUsersController.php
+++ app/Controllers/ConfigurationUsersController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controllers;
 
+use App\Services\Store;
 use App\Services\SesionService;
 use App\Middlewares\Permissions;
 use App\Controllers\ConfigurationPermissionsController;
