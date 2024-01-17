5b5e2026d2a1803c6c1d4f4baec908715cd5093d
codigoinerte <fmartinez.bpe@gmail.com>
2024-01-17 12:53:05 -0500

    fix: cambio de default para URL_WEB

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ $dotenv->load();
 
 define("URL_APP", URL_ROOT . "app");
 define("APP_BASE_EP", $_SERVER["APP_BASE_EP"]??"https://services-testing.babilonia.io/");
-define("URL_WEB", $_SERVER["URL_WEB"]??"https://www-testing.babilonia.io/");
+define("URL_WEB", $_SERVER["URL_WEB"]??"https://app-testing.babilonia.io/");
 define("URL_LOGS", URL_ROOT. "logs/");
 define("URL_PUBLIC", URL_ROOT . "public/");
 define("URL_ASSETS", URL_ROOT . "public/assets/");
