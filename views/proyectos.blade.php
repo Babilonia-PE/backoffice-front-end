44a6fb3d77ea0c1c0e39edebb7f05a964867ded5
Ivan Rivas Peña <ivan.rivas@babilonia.io>
2024-05-23 16:11:22 -0500

    Se agrego campo source en vista avisos, y se implemento la descarga en la vista proyectos

--- views/proyectos.blade.php
+++ views/proyectos.blade.php
@@ -467,7 +467,9 @@ Proyectos
 	}
 	const columnsHidden = [2, 8, 9, 10, 11, 12, 14, 15, 16, 17, 18, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43];
 	const columnsDates = [37, 38, 39, 40];
+	const download = { active: true, filename: 'Proyectos.xlsx' };
 	const options = {
+		download,
 		processParams,
 		headers,
 		filtersFields,
