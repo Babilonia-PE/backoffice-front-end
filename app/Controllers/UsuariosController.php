37f944b845b1a56c82b240b75fd2fef1b8a35dd0
codigoinerte <fmartinez.bpe@gmail.com>
2024-08-28 14:07:22 -0500

    feat: implementación de permisos de usuario logueado en vista Usuarios

--- app/Controllers/UsuariosController.php
+++ app/Controllers/UsuariosController.php
@@ -6,7 +6,9 @@ use App\Middlewares\Permissions;
 class UsuariosController extends Permissions{
 
     function index(){
+        $permissions = $this->getUserPermissions();
         echo view("usuarios", [
+            "permissions" => $permissions,
             "currentPage" => "UsuariosController"
          ]);
     }
