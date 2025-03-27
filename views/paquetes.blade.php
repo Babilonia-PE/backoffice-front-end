215768de08de901895e923993c0b1624320e7821
Ivan Rivas Peña <ivan.rivas@babilonia.io>
2025-03-27 10:10:35 -0500

    [REFACTOR]: Ticket 1354

--- views/paquetes.blade.php
+++ views/paquetes.blade.php
@@ -137,7 +137,7 @@ Paquetes
 
 <div class="row">
 	<div class="col-12">
-        <div id="filter_box" class="card collapsed-card">
+        <div id="filter_box" class="card">
 			<div class="card-header" role="button" data-card-widget="collapse">
                 <h5 class="card-title">Filtros de búsqueda</h5>
                 <div class="card-tools">
@@ -275,7 +275,7 @@ Paquetes
 			</div>
         </div>
 	</div>
-    <div class="col-12">
+    <div id="table-box" class="col-12 d-none">
         <div class="card">
             <div class="card-body table-responsive">
 				<p class="text-center" name="loading"><img src="public/assets/img/loading.gif" width="50" /></p>
--- views/usuarios.blade.php
+++ views/usuarios.blade.php
@@ -120,7 +120,7 @@ Usuarios
 
 <div class="row">
 	<div class="col-12">
-        <div id="filter_box" class="card collapsed-card">
+        <div id="filter_box" class="card">
 			<div class="card-header" role="button" data-card-widget="collapse">
                 <h5 class="card-title">Filtros de búsqueda</h5>
                 <div class="card-tools">
@@ -185,7 +185,7 @@ Usuarios
 			</div>
         </div>
 	</div>
-    <div class="col-12">
+    <div id="table-box" class="col-12 d-none">
         <div class="card">
             <div class="card-body table-responsive table-usuarios">
 				<p class="text-center" name="loading"><img src="public/assets/img/loading.gif" width="50" /></p>
