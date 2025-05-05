daef47560b6bc2c3a9d5a0bcdced705ee06c2756
Ivan Rivas Peña <ivan.rivas@verticalcapital.io>
2025-05-05 13:18:23 -0500

    [REFACTOR]: Ticket 1426

--- views/views-projects.blade.php
+++ views/views-projects.blade.php
@@ -155,10 +155,10 @@ Views Proyectos
 							<label for="exampleInputEmail1">Fecha de creación (Desde - Hasta)</label>
 							<div class="form-row">
 								<div class="col-6">
-									<input type="text" class="form-control form-control-sm" id="created_start" placeholder="dd/mm/yyyy" value="{{date('d-m-Y', strtotime('-3 month'))}}">
+									<input type="text" class="form-control form-control-sm" id="created_start" placeholder="dd/mm/yyyy" >
 								</div>
 								<div class="col-6">
-									<input type="text" class="form-control form-control-sm" id="created_end" placeholder="dd/mm/yyyy" value="{{date('d-m-Y')}}">
+									<input type="text" class="form-control form-control-sm" id="created_end" placeholder="dd/mm/yyyy" >
 								</div>
 							</div>
 						</div>
